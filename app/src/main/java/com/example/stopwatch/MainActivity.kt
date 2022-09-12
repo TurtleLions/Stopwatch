2191ca22e1abeca03feca1aeec842d9c4cae96c8
TurtleLions <ryanestanislao@gmail.com>
2022-09-12 08:36:41 -0700

    override methods

--- app/src/main/java/com/example/stopwatch/MainActivity.kt
+++ app/src/main/java/com/example/stopwatch/MainActivity.kt
@@ -3,14 +3,19 @@ package com.example.stopwatch
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.Chronometer
 
 class MainActivity : AppCompatActivity() {
 
     companion object{
         val TAG = "MainActivity"
     }
-
+    private lateinit var startButton: Button
+    private lateinit var resetButton: Button
+    private lateinit var timer: Chronometer
     override fun onCreate(savedInstanceState: Bundle?) {
+        wireWidgets()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
@@ -18,5 +23,27 @@ class MainActivity : AppCompatActivity() {
     override fun onStart() {
         super.onStart()
         Log.d(TAG, "onStart: ")
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+    private fun wireWidgets(){
+        startButton = findViewById(R.id.button_main_start)
+        resetButton = findViewById(R.id.button_main_reset)
+        timer = findViewById(R.id.chronometer_main_stopwatch)
     }
 }
